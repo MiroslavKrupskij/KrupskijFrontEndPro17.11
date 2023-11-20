2e189cb83165c7a2010eafb32b213081cb18f698
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-11-20 12:50:59 +0200

    Homework3

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-console.log()
+const рядок1 = prompt("Введіть перший рядок:");
+const рядок2 = prompt("Введіть другий рядок:");
+const рядок3 = prompt("Введіть третій рядок:");
 
-alert(`Hello world!`)
+const result = рядок3.concat(рядок1, рядок2);
+
+alert (result);
+
+console.log({ result });
+
+const number = prompt("Введіть п'ятизначне число:");
+
+const result2 = number.split('').join(' ');
+
+alert (result2);
+
+console.log({ result2 });
