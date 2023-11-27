91f195584db0616679b3596fe9323deff08279e8
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-11-27 17:07:22 +0200

    Homework8

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const години = prompt("Введіть кількість годин:");
+var first_number = prompt("Введіть перше число:");
 
-var секунди = години * 3600;
+var second_number = prompt("Введіть друге число:");
 
-alert(`${години} годин(и) в секундах = ${секунди}`);
+var third_number = prompt("Введіть третє число:");
+
+first_number = parseFloat(first_number);
+
+second_number = parseFloat(second_number);
+
+third_number = parseFloat(third_number);
+
+var result = (first_number + second_number + third_number) / 3;
+
+alert(`Середнє арифметичне чисел ${first_number}, ${second_number} і ${third_number} = ${result}`);
 
 console.log()
