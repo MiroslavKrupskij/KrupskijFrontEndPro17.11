a018dc258363d73d9df0a33735c0f86efd4feae8
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-11-28 07:48:12 +0200

    homework 9

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var first_number = prompt("Введіть перше число:");
+const numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-var second_number = prompt("Введіть друге число:");
-
-var third_number = prompt("Введіть третє число:");
-
-first_number = parseFloat(first_number);
-
-second_number = parseFloat(second_number);
-
-third_number = parseFloat(third_number);
-
-var result = (first_number + second_number + third_number) / 3;
-
-alert(`Середнє арифметичне чисел ${first_number}, ${second_number} і ${third_number} = ${result}`);
-
-console.log()
+switch (numOrStr) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+        break;
+}
