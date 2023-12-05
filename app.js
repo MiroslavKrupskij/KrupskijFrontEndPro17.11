7abe1f803c10f02be592f646007bbb90fe358c1f
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-12-05 13:56:43 +0200

    Homework12

--- app.js
+++ app.js
@@ -1,58 +1,18 @@
-// 1
-for (let a = 20; a <= 30; a += 0.5) {
-    console.log(a);
+const arrayLength = parseFloat(prompt("Введіть довжину масиву:"));
+
+let array = [];
+
+for (let i = 0; i < arrayLength; i++) {
+    const element = parseFloat(prompt("Введіть елемент масиву:"));
+    array.push(element);
 }
 
-// 2
-const d = 27;
-for (let k = 10; k <= 100; k += 10) {
-    console.log(`${k}$ = ${k * d}₴`)
-}
+console.log("Введений масив", array);
 
-// 3
-const number = 3782;
-for (let a = 1; a <= 100; a++) {
-    if(a * a <= number) {
-        console.log(a)
-    }
-}
+array.sort((a, b) => a - b);
 
-// 4 
-function is_prime(number2) {
-    if (number2 <= 1) {
-        return false; 
-    }
-    for (let d = 2; d <= Math.sqrt(number2); d++) {
-        if (number2 % d === 0) {
-            return false; 
-        }
-    }
-    return true; 
-}
-const input_number = prompt(`Введіть число: `)
+console.log("Відсортований масив:", array);
 
-if (is_prime(input_number)) {
-    console.log(`${input_number}  є простим числом.`);
-} else {
-    console.log(`${input_number} не є простим числом.`);
-} 
+array.splice(1, 3);
 
-// 5
-function is_log3(num) {
-    if (num <= 0) {
-        return false; 
-    }
-    while (num !== 1) {
-        if (num % 3 !== 0) {
-            return false; 
-        }
-        num /= 3;
-    }
-    return true; 
-}
-const num = prompt(`Введіть число:`); 
-if (is_log3(num)) {
-    console.log(`${num} можна отримати зведенням числа 3 у деякий ступінь.`);
-} else {
-    console.log(`${num} не можна отримати зведенням числа 3 у деякий ступінь.`);
-}
+console.log("Масив після видалення елементів з 2 по 4:", array);
