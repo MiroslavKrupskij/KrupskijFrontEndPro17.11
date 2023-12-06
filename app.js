4424e078936a2941c4ba8ad02fbb344710a04431
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-12-06 19:36:00 +0200

    Homework15

--- app.js
+++ app.js
@@ -1,97 +1,65 @@
-let array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-
-let positiveSum = 0;
-let positiveCounter = 0;
-
-let minElement = array[0];
-let minIndex = 0;
-
-let maxElement = array[0];
-let maxIndex = 0;
-
-let negativeCounter = 0;
-
-let sumOddPositive = 0;
-let oddPositiveCounter = 0;
-
-let sumEvenPositive = 0;
-let evenPositiveCounter = 0;
-
-let multiplicationPositives = 1;
-
-let maxElementReset = Math.max(...array);
-
-for (let i = 0; i < array.length; i++) {
-    
-    // 1
-
-if (array[i] > 0) {
-    positiveSum += array[i];
-    positiveCounter++;
-}
-
-// 2
-
-if (array[i] < minElement) {
-    minElement = array[i];
-    minIndex = i;
-}
-
-// 3
-
-if (array[i] > maxElement) {
-    maxElement = array[i];
-    maxIndex = i;
-}
-
-// 4
-
-if (array[i] < 0) {
-    negativeCounter++;
-}
-
-// 5 
-
-if (array[i] > 0) {
-    if (array[i] % 2 === 0) {
-        evenPositiveCounter++;
-        sumEvenPositive += array[i];
-    } else {
-        oddPositiveCounter++;
-        sumOddPositive += array[i];
+let users = [
+    {
+    "index": 0,
+    "isActive": true,
+    "balance": "$2,226.60",
+    "name": "Eugenia Sawyer",
+    "gender": "female",
+    "phone": "+1 (840) 583-3207",
+    "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+    "index": 1,
+    "isActive": true,
+    "balance": "$2,613.77",
+    "name": "Pauline Gallegos",
+    "gender": "female",
+    "phone": "+1 (985) 593-3328",
+    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+    "index": 2,
+    "isActive": false,
+    "balance": "$3,976.41",
+    "name": "Middleton Chaney",
+    "gender": "male",
+    "phone": "+1 (995) 591-2478",
+    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+    "index": 3,
+    "isActive": true,
+    "balance": "$1,934.58",
+    "name": "Burns Poole",
+    "gender": "male",
+    "phone": "+1 (885) 559-3422",
+    "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+    "index": 4,
+    "isActive": true,
+    "balance": "$3,261.65",
+    "name": "Mcfadden Horne",
+    "gender": "male",
+    "phone": "+1 (942) 565-3988",
+    "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+    "index": 5,
+    "isActive": false,
+    "balance": "$1,790.56",
+    "name": "Suzette Lewis",
+    "gender": "female",
+    "phone": "+1 (837) 586-3283",
+    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
     }
-}
+    ]
 
-// 6
+    let balances = users.map(user => parseFloat(user.balance.replace('$', '').replace(',', '')));
 
-if (array[i] > 0) {
-    multiplicationPositives *= array[i];
-}
-
-//7
-
-if (array[i] != maxElementReset) {
-    array[i] = 0;
-}
-
-}
-
-console.log(`Сума позитивних елементів: ${positiveSum}. Їх кількість становить: ${positiveCounter}`);
-
-console.log(`Мінімальний елемент: ${minElement}. Його індекс: ${minIndex}`);
-
-console.log(`Максимальний елемент: ${maxElement}. Його індекс: ${maxIndex}`);
-
-console.log(`Кількість негативних елементів: ${negativeCounter}`);
-
-console.log(`Кількість непарних позитивних елементів: ${oddPositiveCounter}`);
-
-console.log(`Сума непарних позитивних елементів: ${sumOddPositive}`);
-
-console.log(`Сума парних позитивних елементів: ${sumEvenPositive}`);
-
-console.log(`Кількість парних позитивних елементів: ${evenPositiveCounter}`);
-
-console.log(`Добуток позитивних елементів: ${multiplicationPositives}`);
-
-console.log(`Найбільший елемент з обнуленням інших: ${array}`);
+    let highBalance = users.filter(user => parseFloat(user.balance.replace('$', '').replace(',', '')) > 2000);
+    
+    let totalBalance = balances.reduce((sum, balance) => sum + balance, 0);
+    
+    console.log(`Телефонні номери користувачів з балансом більше, ніж 2000: ${highBalance.map(user => user.phone)}`);
+    console.log(`Загальний баланс всіх користувачів: ${totalBalance.toFixed(2)}`);
