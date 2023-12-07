a71018d136b93d7cc4faa947a99e4c16d1863929
Miroslav Krupskij <miroslavkrupskij@gmail.com>
2023-12-07 18:32:38 +0200

    HW

--- app.js
+++ app.js
@@ -1,65 +1,16 @@
-let users = [
-    {
-    "index": 0,
-    "isActive": true,
-    "balance": "$2,226.60",
-    "name": "Eugenia Sawyer",
-    "gender": "female",
-    "phone": "+1 (840) 583-3207",
-    "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-    "index": 1,
-    "isActive": true,
-    "balance": "$2,613.77",
-    "name": "Pauline Gallegos",
-    "gender": "female",
-    "phone": "+1 (985) 593-3328",
-    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-    "index": 2,
-    "isActive": false,
-    "balance": "$3,976.41",
-    "name": "Middleton Chaney",
-    "gender": "male",
-    "phone": "+1 (995) 591-2478",
-    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-    "index": 3,
-    "isActive": true,
-    "balance": "$1,934.58",
-    "name": "Burns Poole",
-    "gender": "male",
-    "phone": "+1 (885) 559-3422",
-    "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-    "index": 4,
-    "isActive": true,
-    "balance": "$3,261.65",
-    "name": "Mcfadden Horne",
-    "gender": "male",
-    "phone": "+1 (942) 565-3988",
-    "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-    "index": 5,
-    "isActive": false,
-    "balance": "$1,790.56",
-    "name": "Suzette Lewis",
-    "gender": "female",
-    "phone": "+1 (837) 586-3283",
-    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+function removeElement(array, item) {
+
+    const index = array.indexOf(item);
+  
+    if (index >= 0) {
+      array.splice(index, 1);
     }
-    ]
+  
+    return array;
+  }
 
-    let balances = users.map(user => parseFloat(user.balance.replace('$', '').replace(',', '')));
-
-    let highBalance = users.filter(user => parseFloat(user.balance.replace('$', '').replace(',', '')) > 2000);
-    
-    let totalBalance = balances.reduce((sum, balance) => sum + balance, 0);
-    
-    console.log(`Телефонні номери користувачів з балансом більше, ніж 2000: ${highBalance.map(user => user.phone)}`);
-    console.log(`Загальний баланс всіх користувачів: ${totalBalance.toFixed(2)}`);
+  removeElement(array2, 12);
+  
+  console.log(array2);
